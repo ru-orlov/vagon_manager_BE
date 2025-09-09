d2cf6a61e09ff67cfb2cba2c1958d1d369e7c8a5
rorlov <info@dahushim.com>
2025-09-09 15:36:36 +0300

    - init commit

---
+++ src/main/java/com/example/wagonmanager/WagonmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.wagonmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagonmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagonmanagerApplication.class, args);
+	}
+
+}
